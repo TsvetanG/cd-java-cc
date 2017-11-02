bb74ff8841457e8586924051baf4151173ea1dcb
TsvetanG <tsvetan.georgiev@gmail.com>
2017-11-01 23:15:49 -0400

    cleanup

--- src/main/java/com/example/cc/TransferChaincode.java
+++ src/main/java/com/example/cc/TransferChaincode.java
@@ -1,3 +1,16 @@
+/**
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ *  DO NOT USE IN PROJECTS , NOT for use in production
+ */
 package com.example.cc;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -28,8 +41,7 @@ public class TransferChaincode extends ChaincodeBase {
     switch (operation) {
     case "query":
       return query(stub, arguments.stream().toArray(String[]::new));
-    case "transfer":
-      byte[] bytes = new byte[1];
+    case "transfer": 
       stub.setEvent("event",  arguments.get(1).getBytes());
       return transfer(stub, arguments.stream().toArray(String[]::new));
       
