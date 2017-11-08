03df5ab560d4ef19ebe1c3e4da9fb26ea5b53641
TsvetanG <tsvetan.georgiev@gmail.com>
2017-11-08 18:31:39 -0500

    Update add path to shim jar for Eclipse

--- src/main/java/com/example/cc/TransferChaincode.java
+++ src/main/java/com/example/cc/TransferChaincode.java
@@ -41,32 +41,35 @@ public class TransferChaincode extends ChaincodeBase {
     switch (operation) {
     case "query":
       return query(stub, arguments.stream().toArray(String[]::new));
-    case "transfer": 
-      stub.setEvent("event",  arguments.get(1).getBytes());
+    case "transfer":
+
+      stub.setEvent("event", "ChainCodeEvent".getBytes());
       return transfer(stub, arguments.stream().toArray(String[]::new));
-      
+
     default:
-      break; 
+      break;
     }
     return newErrorResponse(Json.createObjectBuilder().add("Error", "Unknown operation").build().toString());
   }
 
   protected Response transfer(ChaincodeStub stub, String[] args) {
-    
-    int fromBalance= Integer.parseInt(stub.getStringState(args[1]));
+
+    int fromBalance = Integer.parseInt(stub.getStringState(args[1]));
     int toBalance = Integer.parseInt(stub.getStringState(args[2]));
     int amount = Integer.parseInt(args[3]);
-     
-    stub.putStringState(args[1], String.valueOf( fromBalance - amount));
-    stub.putStringState(args[2], String.valueOf( toBalance  + amount));
-    return newSuccessResponse(); 
+
+    stub.putStringState(args[1], String.valueOf(fromBalance - amount));
+    stub.putStringState(args[2], String.valueOf(toBalance + amount));
+    return newSuccessResponse();
   }
 
   protected Response initialize(ChaincodeStub stub, String[] args) {
+    
     StringBuilder builder = new StringBuilder();
     for (int i = 0; i < args.length; i++) {
       builder.append("<").append(args[i]).append(">");
     }
+    
     stub.putStringState("INIT", builder.toString());
     String acc1 = args[1];
     String acc1Balance = args[2];
@@ -78,19 +81,21 @@ public class TransferChaincode extends ChaincodeBase {
 
     return newSuccessResponse();
   }
-  
+
   protected Response query(ChaincodeStub stub, String[] args) {
+
+
     JsonObjectBuilder builder = Json.createObjectBuilder();
     String accountKey = args[1];
     int i;
     try {
       i = Integer.parseInt(stub.getStringState(accountKey));
-    } catch (Exception e) { 
+    } catch (Exception e) {
       builder.add("Name", accountKey).add("Amount", e.getMessage());
       return newSuccessResponse(builder.build().toString().getBytes(UTF_8));
     }
-    builder.add("Name", accountKey).add("Amount", i );
-    return newSuccessResponse( builder.build().toString().getBytes(UTF_8));
+    builder.add("Name", accountKey).add("Amount", i);
+    return newSuccessResponse(builder.build().toString().getBytes(UTF_8));
 
   }
 
